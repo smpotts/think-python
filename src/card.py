41be9e422f3a8900d938bdc28772ea5b96e40b9f
Shelby Potts <shelbypotts.connect@gmail.com>
2024-06-16 08:07:46 -0500

    solution code

--- src/card.py
+++ src/card.py
@@ -1,22 +1,36 @@
 class Card:
-    # instance attributes
-    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7',
-                  '8', '9', '10', 'Jack', 'Queen', 'King']
+    """Represents a standard playing card.
+
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7",
+                  "8", "9", "10", "Jack", "Queen", "King"]
 
     def __init__(self, suit=0, rank=2):
-        # defaults to a 2 of Clubs
-
-        # class attributes
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
+        """Returns a human-readable string representation."""
         return '%s of %s' % (Card.rank_names[self.rank],
                              Card.suit_names[self.suit])
 
+    def __eq__(self, other):
+        """Checks whether self and other have the same rank and suit.
+
+        returns: boolean
+        """
+        return self.suit == other.suit and self.rank == other.rank
+
     def __lt__(self, other):
-        # using tuple comparison
+        """Compares this card to other, first by suit, then rank.
+
+        returns: boolean
+        """
         t1 = self.suit, self.rank
         t2 = other.suit, other.rank
         return t1 < t2
--- src/hand.py
+++ src/hand.py
@@ -1,12 +1,34 @@
 from src.deck import Deck
 
-
 class Hand(Deck):
+    """Represents a hand of playing cards."""
 
-    def init__(self, label=''):
+    def __init__(self, label=''):
         self.cards = []
         self.label = label
 
-    def move_cards(self, hand, num):
-        for i in range(num):
-            hand.add_card(self.pop_card())
+
+def find_defining_class(obj, method_name):
+    """Finds and returns the class object that will provide 
+    the definition of method_name (as a string) if it is
+    invoked on obj.
+
+    obj: any python object
+    method_name: string method name
+    """
+    for ty in type(obj).mro():
+        if method_name in ty.__dict__:
+            return ty
+    return None
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    deck.shuffle()
+
+    hand = Hand()
+    print(find_defining_class(hand, 'shuffle'))
+
+    deck.move_cards(hand, 5)
+    hand.sort()
+    print(hand)
